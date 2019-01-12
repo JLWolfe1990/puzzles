3683fa0090ed81398ff8aaeccdc2a97e8a08967d
Joshua Wolfe <jlwolfe1990@gmail.com>
2019-01-12 12:55:59 -0600

    Add basic working example

---
+++ phone_fun.rb
@@ -0,0 +1,49 @@
+class CodeTestException < Exception; end
+
+# driver method
+def phone_format(s)
+  string = s.to_s.gsub(/[^0-9]/, '') # force input to string
+  string_length = string.length
+
+  # ensure that the return type stays consistent and make sure that there isn't
+  # one digit by itself as specified by the API
+  return string unless string_length > 2
+
+  format_phone(string, string_length)
+end
+
+private
+
+def format_phone(string, string_length)
+  formatted_string = ""
+  early_dash = string_length % 3 == 1
+
+  # start index at 1 for easier comprehension
+  i = 0
+
+  string.each_char do |char|
+    formatted_string << char
+    break if i == string_length
+
+    i += 1
+
+    if early_dash && (i == string_length - 2)
+      # jump to the end and break
+
+      formatted_string << '-'
+      formatted_string << string[-2..-1].to_s
+
+      break
+    end
+
+    formatted_string << '-' if i % 3 == 0
+  end
+
+  formatted_string
+end
+
+raise CodeTestException unless phone_format("(+1) 888 33x19") == "188-833-19"
+raise CodeTestException unless phone_format("555 123 1234") == "555-123-12-34"
+raise CodeTestException unless phone_format("(+1)") == '1'
+raise CodeTestException unless phone_format(nil) == ""
+raise CodeTestException unless phone_format("") == ""
